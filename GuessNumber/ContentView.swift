55ba597be19d35658b7caa9f282729feb84036ca
KremnevViacheslav <rusted2192@gmail.com>
2022-05-04 19:08:46 +0300

    Initial Commit

---
+++ GuessNumber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessNumber
+//
+//  Created by Вячеслав Кремнев on 04.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuessNumber/GuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberApp.swift
+//  GuessNumber
+//
+//  Created by Вячеслав Кремнев on 04.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
